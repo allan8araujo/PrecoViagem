2ba2f18b35e6284e5f87ad3aa0e6118226895f2d
allan8araujo@gmail.com <allan8araujo>
2022-04-04 07:36:21 -0300

    Implementando onClickListener

--- app/src/main/java/com/example/precoviagem/MainActivity.kt
+++ app/src/main/java/com/example/precoviagem/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.precoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.precoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,8 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.buttonCalculate.setOnClickListener(this)
 
+    }
 
-        binding.editDistance
+    fun calculate (){
+        Toast.makeText(this,"fui clicado!",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onClick(view: View) {
+        if (view.id==R.id.button_calculate){
+            calculate()
+        }
     }
 }
